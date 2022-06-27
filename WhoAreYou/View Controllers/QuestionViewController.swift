6f1d0b04acfeb23ca9b8b605bfd41d140b63f09e
DimaRyndya <dmitriy.ryndya@gmail.com>
2022-06-27 18:31:15 +0300

    Initial commit

---
+++ WhoAreYou/View Controllers/QuestionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  QuestionViewController.swift
+//  WhoAreYou
+//
+//  Created by Dmitriy Ryndya on 06.06.2022.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+    
+}
+
+
---
+++ WhoAreYou/View Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhoAreYou
+//
+//  Created by Dmitriy Ryndya on 06.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
